a310f35697f0aa39b3235c25daac92dfc2ea1884
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-08 19:38:25 +0100

    DiceRollMultiple
    Signed-off-by: ElectroluxV2 <electroluxv2@gmail.com>

--- exercise-3/subsection-2.php
+++ exercise-3/subsection-2.php
@@ -1,1 +1,20 @@
 <?php
+// Zmodyfikuj funkcję z zadania 1.1, by przyjmowała argument - liczbę rzutów kostką. I
+// zwracała tablicę wyników.
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+function DiceRollMultiple($numberOfThrows): array {
+    if ($numberOfThrows <= 0) throw new Error('Parameter numberOfThrows must be greater than 0!');
+
+    $result = [];
+    for ($i = 0; $i < $numberOfThrows; $i++)
+        array_push($result, rand(1, 6));
+    return $result;
+}
+
+$throws = DiceRollMultiple(rand(1, '6'));
+highlight_string("<?php\n\$throws =\n" . var_export($throws, true) . ";\n?>\n");
+$throws = DiceRollMultiple(rand(1, 'z'));
