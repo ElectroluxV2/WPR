574fdd8374f51812e7ca45d50f851b9542e002e8
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-08 18:24:38 +0100

    Fixed wrong return type
    Signed-off-by: ElectroluxV2 <electroluxv2@gmail.com>

--- exercise-1/subsection-2.php
+++ exercise-1/subsection-2.php
@@ -5,13 +5,13 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-function CalcDiameter($radius): int {
+function CalcDiameter($radius): float {
     if (!is_numeric($radius)) throw new Error('Parameter radius must be numeric type!');
     if ($radius < 0) throw new Error('Parameter radius must be greater than 0!');
     return 2 * $radius;
 }
 
-echo CalcDiameter(1).'<br>';
+echo CalcDiameter(1.1).'<br>';
 echo CalcDiameter('1').'<br>';
 echo CalcDiameter(-2).'<br>';
 echo CalcDiameter('NAN').'<br>';
