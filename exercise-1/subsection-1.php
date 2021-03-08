c3f72758f32c93f2d8ada6105b884ee558a77923
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-08 18:05:27 +0100

    DiceRoll
    Signed-off-by: ElectroluxV2 <electroluxv2@gmail.com>

--- exercise-1/subsection-1.php
+++ exercise-1/subsection-1.php
@@ -1,1 +1,16 @@
 <?php
+// Napisz funkcję zwracającą wynik - symulację rzutu kostką.
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+function DiceRoll(): int {
+    return rand(1, 6);
+}
+
+echo DiceRoll().'<br>';
+echo DiceRoll().'<br>';
+echo DiceRoll().'<br>';
+echo DiceRoll().'<br>';
+echo DiceRoll().'<br>';
