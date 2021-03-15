fc2a59e04d32e62b3709da3affe93dd41dfa3a7f
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-15 16:33:15 +0100

    Harder version
    Signed-off-by: ElectroluxV2 <electroluxv2@gmail.com>

--- exercise-1/subsection-4.php
+++ exercise-1/subsection-4.php
@@ -10,11 +10,33 @@ function ReadDateFromId($id): string {
     if (!is_string($id)) throw new Error('Parameter id must be string type!');
     if (!ctype_digit($id)) throw new Error('Parameter id must contain only digits!');
 
-    $YY = substr($id, 0, 2);
-    $MM = substr($id, 2, 2);
-    $DD = substr($id, 4, 2);
+    $month = substr($id,2,2);
+    $day = substr($id,4,2);
 
-    return $DD.'-'.$MM.'-'.$YY;
+    $additionalMonths = [80, 0, 20, 40, 60];
+    $monthsBase = range(1,12);
+    $months = [];
+    foreach ($additionalMonths as $additionalMonth) {
+        foreach ($monthsBase as $monthBase) {
+            $months[] = $additionalMonth + $monthBase;
+        }
+    }
+
+    if (!in_array($month, $months)) throw new Error('Invalid date');
+
+    $century = 0;
+    if (substr($month,0,1)=='0' || substr($month,0,1)=='1') $century = 1900;
+    if (substr($month,0,1)=='8' || substr($month,0,1)=='9') $century = 1800;
+    if (substr($month,0,1)=='2' || substr($month,0,1)=='3') $century = 2000;
+    if (substr($month,0,1)=='5' || substr($month,0,1)=='4') $century = 2100;
+    if (substr($month,0,1)=='6' || substr($month,0,1)=='7') $century = 2200;
+    if ($century == '2000') $month = $month - 20;
+    if ($century == '1800') $month = $month - 80;
+    if ($century == '2100') $month = $month - 40;
+    if ($century == '2200') $month = $month - 60;
+
+    $year = $century + substr($id,0,2);
+    return $day.'-'.$month.'-'.$year;
 }
 
 echo ReadDateFromId('13242200705').'<br>';
