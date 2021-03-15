eb79bdb173d9dae3609727e6a1a354a7ef324ac6
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-15 16:34:33 +0100

    Fixed spacing
    Signed-off-by: ElectroluxV2 <electroluxv2@gmail.com>

--- exercise-1/subsection-4.php
+++ exercise-1/subsection-4.php
@@ -25,11 +25,11 @@ function ReadDateFromId($id): string {
     if (!in_array($month, $months)) throw new Error('Invalid date');
 
     $century = 0;
-    if (substr($month,0,1)=='0' || substr($month,0,1)=='1') $century = 1900;
-    if (substr($month,0,1)=='8' || substr($month,0,1)=='9') $century = 1800;
-    if (substr($month,0,1)=='2' || substr($month,0,1)=='3') $century = 2000;
-    if (substr($month,0,1)=='5' || substr($month,0,1)=='4') $century = 2100;
-    if (substr($month,0,1)=='6' || substr($month,0,1)=='7') $century = 2200;
+    if (substr($month,0,1) == '0' || substr($month,0,1) == '1') $century = 1900;
+    if (substr($month,0,1) == '8' || substr($month,0,1) == '9') $century = 1800;
+    if (substr($month,0,1) == '2' || substr($month,0,1) == '3') $century = 2000;
+    if (substr($month,0,1) == '5' || substr($month,0,1) == '4') $century = 2100;
+    if (substr($month,0,1) == '6' || substr($month,0,1) == '7') $century = 2200;
     if ($century == '2000') $month = $month - 20;
     if ($century == '1800') $month = $month - 80;
     if ($century == '2100') $month = $month - 40;
