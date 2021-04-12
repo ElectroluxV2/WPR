9d7a3b18d30d559c50351d613b2bccff63c96c95
ElectroluxV2 <electroluxv2@gmail.com>
2021-04-12 13:19:44 +0200

    Exercise 1 final
    Signed-off-by: ElectroluxV2 <electroluxv2@gmail.com>

--- w4/exercise-1/subsection-1.php
+++ w4/exercise-1/subsection-1.php
@@ -1,57 +1,16 @@
 <?php declare(strict_types=1);
 // Przerób zadanie z poprzednich zajęć, kalkulator, tak aby:
-// każde działanie (dodawanie, odejmowanie itp) było umieszczone w oddzielnej funkcji
+// wszystkie funkcje były umieszczone w innym pliku niż główny skrypt
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-function PrintForm(float $a, float $b, float $result): void {
-    echo <<<EOL
-<form method="post" name="calc">
-    <fieldset>
-    
-        <input type="number" step="0.01" name="a" value="${a}" required>
-        
-        <label for="+">+</label>
-        <input type="radio" name="operator" value="+" id="+" required>
-        
-        <label for="-">-</label>
-        <input type="radio" name="operator" value="-" id="-">
-        
-        <label for=":">:</label>
-        <input type="radio" name="operator" value="/" id=":">
-        
-        <label for="*">*</label>
-        <input type="radio" name="operator" value="*" id="*">
-        
-        <input type="number" step="0.01" name="b" value="${b}" required>
-    
-        <input type="submit" value="=">
-        
-        <code>${result}</code>
-    
-    </fieldset>
-</form>
-EOL;
-}
-
-function Add(float $a, float $b): float {
-    return $a + $b;
-}
-
-function Subtract(float $a, float $b): float {
-    return $a - $b;
-}
-
-function Multiply(float $a, float $b): float {
-    return $a * $b;
-}
-
-function Divide(float $a, float $b): float {
-    return $a / $b;
-}
-
+include_once "add.php";
+include_once "divide.php";
+include_once "multiply.php";
+include_once "printForm.php";
+include_once "subtract.php";
 
 $result = null;
 $a = 0;
